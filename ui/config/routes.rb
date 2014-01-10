714a064c6f689ecc53bec3f91f35427a868490d6
Alexey Melezhik <melezhik@gmail.com>
2014-01-10 17:46:38 +0400

    navigation-bar

--- ui/config/routes.rb
+++ ui/config/routes.rb
@@ -1,15 +1,16 @@
 Ui::Application.routes.draw do
+
   get "projects/new"
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   resources :projects
  
-  root to: "welcome#index"
+  # root to: "welcome#index"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
