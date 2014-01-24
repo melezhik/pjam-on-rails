17ebaed2c08a5aab9f55805cf2b4cb982b383c30
Alexey Melezhik <melezhik@gmail.com>
2014-01-24 16:17:00 +0400

    downloads-urls

--- ui/app/models/build.rb
+++ ui/app/models/build.rb
@@ -5,10 +5,5 @@ class Build < ActiveRecord::Base
         "builds/#{id}"
     end
 
-
-    def download_path
-
-        "/data/#{project.id}/builds/#{id}/artefacts/#{distribution_name}"
-    end
 end
 
