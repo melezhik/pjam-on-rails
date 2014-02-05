cfedae56e7c2cb3a04f68737a2667df7e1a7004c
Alexey Melezhik <melezhik@gmail.com>
2014-02-05 11:36:37 +0400

    jabber_server_delete

--- ui/db/migrate/20140131132726_add_notify_column_to_settings.rb
+++ ui/db/migrate/20140131132726_add_notify_column_to_settings.rb
@@ -1,6 +1,5 @@
 class AddNotifyColumnToSettings < ActiveRecord::Migration
   def change
-    add_column :settings, :jabber_server, :string
     add_column :settings, :jabber_login, :string
     add_column :settings, :jabber_password, :string
   end
