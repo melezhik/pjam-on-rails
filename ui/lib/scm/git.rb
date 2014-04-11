58522ebf6351cc87e0a52d51fa9d3795953e71dc
Alexey Melezhik <melezhik@gmail.com>
2014-04-11 12:55:48 +0400

    scm-refactroing

--- ui/lib/scm/git.rb
+++ ui/lib/scm/git.rb
@@ -1,7 +1,7 @@
 class SCM::Git < Struct.new( :component, :path )
 
     def last_revision
-        cmd = "cd #{path} && git log -1 --pretty=format:'%h' 2>&1"
+        cmd = "cd #{path}/git-repo/ && git log -1 --pretty=format:'%h' 2>&1"
         unless component[:git_folder].nil?
             cmd << " #{component[:git_folder]}"
         end
@@ -9,7 +9,7 @@ class SCM::Git < Struct.new( :component, :path )
     end
 
     def changes_cmd revision
-        cmd = "cd #{path} && git log #{component.revision} #{revision}"
+        cmd = "cd #{path}/git-repo/ && git log #{component.revision} #{revision}"
         unless component[:git_folder].nil?
             cmd << " #{component[:git_folder]}"
         end
@@ -17,9 +17,11 @@ class SCM::Git < Struct.new( :component, :path )
     end
 
     def checkout_cmd
-        cmd = "git clone -b #{component[:git_branch] || 'master'} #{component.url} #{path}"
-        unless component[:git_folder].nil?
-            cmd << " && ls -l #{path}/#{component[:git_folder]}"
+        cmd = "git clone -b #{component[:git_branch] || 'master'} #{component.url} #{path}/git-repo/"
+        if component[:git_folder].nil?
+            cmd << " && cp -r #{component.url} #{path}/git-repo/*  #{path}/ "
+        else
+            cmd << " && cp -r #{component.url} #{path}/git-repo/#{component[:git_folder]}/*  #{path}/ "
         end
     end
 
